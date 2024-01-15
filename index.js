7e1892e4db9ea6b2d9623c503efee470f87c3178
Lyra0710 <ananyayallapragada@gmail.com>
2024-01-15 22:04:27 +0530

    adding some styling

--- index.js
+++ index.js
@@ -15,7 +15,45 @@ app.use(passport.initialize());
 app.use(passport.session()); 
 
 app.get('/', (req, res) => { 
-    res.send("<button><a href='/auth'>Login With Google</a></button>") 
+    // res.send("<button><a href='/auth'>Login With Google</a></button>") 
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login with Google</title>
+        <style>
+            body {
+                font-family: 'Arial', sans-serif;
+                background-color: #f4f4f4;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                margin: 0;
+            }
+
+            button {
+                background-color: #4285f4;
+                color: #ffffff;
+                padding: 10px 20px;
+                font-size: 16px;
+                border: none;
+                cursor: pointer;
+                text-decoration: none;
+            }
+
+            button:hover {
+                background-color: #357ae8;
+            }
+        </style>
+    </head>
+    <body>
+        <button><a href='/auth' style="text-decoration:none; color:inherit;">Login With Google</a></button>
+    </body>
+    </html>
+`); 
 }); 
 
 app.get('/auth' , passport.authenticate('google', { scope: 
@@ -33,7 +71,7 @@ app.get( '/auth/callback',
 app.get('/auth/callback/success' , (req , res) => { 
     if(!req.user) 
         res.redirect('/auth/callback/failure'); 
-    res.send("Welcome " + req.user.email); 
+    res.send("Welcome  " + req.user.email); 
 }); 
   
 // failure 
